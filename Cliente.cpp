f2192367270fd4fa4f9552852e1fa94a1ffafb3e
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2021-01-09 15:21:55 -0600

    Cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "Cliente.h"
+
+Cliente::Cliente(const string &nombre, const string &id, bool ingresaNinno, bool embarazada, bool adultoMayor,
+                 const string &categoria) : nombre(nombre), id(id), ingresaNinno(ingresaNinno), embarazada(embarazada),
+                                            adultoMayor(adultoMayor), categoria(categoria) {}
+
+Cliente::Cliente() {}
+
+Cliente::~Cliente() {
+
+}
+
+const string &Cliente::getNombre() const {
+    return nombre;
+}
+
+void Cliente::setNombre(const string &nombre) {
+    Cliente::nombre = nombre;
+}
+
+const string &Cliente::getId() const {
+    return id;
+}
+
+void Cliente::setId(const string &id) {
+    Cliente::id = id;
+}
+
+bool Cliente::isIngresaNinno() const {
+    return ingresaNinno;
+}
+
+void Cliente::setIngresaNinno(bool ingresaNinno) {
+    Cliente::ingresaNinno = ingresaNinno;
+}
+
+bool Cliente::isEmbarazada() const {
+    return embarazada;
+}
+
+void Cliente::setEmbarazada(bool embarazada) {
+    Cliente::embarazada = embarazada;
+}
+
+bool Cliente::isAdultoMayor() const {
+    return adultoMayor;
+}
+
+void Cliente::setAdultoMayor(bool adultoMayor) {
+    Cliente::adultoMayor = adultoMayor;
+}
+
+const string &Cliente::getCategoria() const {
+    return categoria;
+}
+
+void Cliente::setCategoria(const string &categoria) {
+    Cliente::categoria = categoria;
+}
--- Cliente.h
+++ Cliente.h
@@ -4,9 +4,47 @@
 
 #ifndef I_PROYECTO_ESTRUCTURAS_CLIENTE_H
 #define I_PROYECTO_ESTRUCTURAS_CLIENTE_H
-
+#include <iostream>
+using namespace std;
 class Cliente {
+private:
+    string nombre;
+    string id;
+    bool ingresaNinno;
+    bool embarazada;
+    bool adultoMayor;
+    string categoria;
+public:
+    Cliente(const string &nombre, const string &id, bool ingresaNinno, bool embarazada, bool adultoMayor,
+            const string &categoria);
 
+    Cliente();
+
+    virtual ~Cliente();
+
+    const string &getNombre() const;
+
+    void setNombre(const string &nombre);
+
+    const string &getId() const;
+
+    void setId(const string &id);
+
+    bool isIngresaNinno() const;
+
+    void setIngresaNinno(bool ingresaNinno);
+
+    bool isEmbarazada() const;
+
+    void setEmbarazada(bool embarazada);
+
+    bool isAdultoMayor() const;
+
+    void setAdultoMayor(bool adultoMayor);
+
+    const string &getCategoria() const;
+
+    void setCategoria(const string &categoria);
 };
 
 
